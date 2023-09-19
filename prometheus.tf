2b0675c71e7fb4d68643f34a92ac05ab974af75b
Tiago Angelo <tiago.angelo@totvs.com.br>
2023-09-19 14:33:35 -0300

    Create 'testkube.tf' for Helm release and namespace configuration

--- prometheus.tf
+++ prometheus.tf
@@ -1,8 +1,10 @@
 resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
+
   name       = "prometheus"
   version    = "25.0.0"
+
   namespace  = kubernetes_namespace_v1.prometheus.metadata[0].name
 
   values = [
---
+++ testkube.tf
@@ -0,0 +1,35 @@
+resource "helm_release" "testkube" {
+  repository = "https://kubeshop.github.io/helm-charts"
+  chart      = "testkube"
+
+  name    = "testkube"
+  version = "1.14.8"
+
+  namespace = kubernetes_namespace_v1.testkube.metadata[0].name
+
+  values = [
+    yamlencode({
+      testkube-api = {
+        uiIngress = {
+          enabled   = true
+          className = "nginx"
+          path      = "/", hosts = ["testkube-api.${local.cluster_host}"]
+        }
+      }
+      testkube-dashboard = {
+        ingress = {
+          enabled   = true
+          className = "nginx"
+          path      = "/", hosts = ["testkube.${local.cluster_host}"]
+        }
+        apiServerEndpoint = "testkube-api.${local.cluster_host}"
+      }
+    })
+  ]
+
+  depends_on = [helm_release.ingress_nginx]
+}
+
+resource "kubernetes_namespace_v1" "testkube" {
+  metadata { name = "testkube" }
+}
